fe96877179fcbc9f1c5955aae47ff82f36465896
tejal2610 <chavantejal308@gmail.com>
2022-06-13 10:34:39 +0530

    Added search functionality for validating iModel against source files

--- Portal/src/components/FilePage/FilePage.tsx
+++ Portal/src/components/FilePage/FilePage.tsx
@@ -5,48 +5,55 @@
 
 import { RouteComponentProps } from '@reach/router';
 import { useEffect, useState } from 'react';
-import {
-  uploadFileToAzureBlob,
-} from '../../services/AzureUpload';
+import { uploadFileToAzureBlob } from '../../services/AzureUpload';
 import './FilePage.scss';
+import { useManifestConnections } from '../../services/ManifestAPI';
 import {
-  useManifestConnections,
-} from '../../services/ManifestAPI';
+  FileUpload,
+  FileUploadTemplate,
+  ProgressLinear,
+} from '@itwin/itwinui-react';
 import { ManifestRunResponse } from '../../dto/Types';
-import { FileUpload, FileUploadTemplate, ProgressLinear } from '@itwin/itwinui-react';
 
 export const FilePage = (props: RouteComponentProps) => {
   const [uploadedFileName, setUploadedFileName] = useState<string | null>();
   const [runState, setRunState] = useState<string | null>();
-  const [createManifestConnectionDefinition, runManifestConnectionDefinition, fetchRunResults] = useManifestConnections();
+  const [
+    fetchConnectionDefinitionId,
+    runManifestConnectionDefinition,
+    fetchRunResults,
+  ] = useManifestConnections(uploadedFileName!);
 
   const fileUpload = async (fileList: FileList) => {
     // Upload file to azure blob storage
-    setRunState("Uploading");
+    setRunState('Uploading');
     const fileName: string | null = await uploadFileToAzureBlob(fileList[0]);
     setUploadedFileName(fileName);
   };
 
-  const manifestConnectionRun = async () => {
-    // Create Manifest connection definition
-    setRunState("Creating connection");
-    const createResponse = await createManifestConnectionDefinition();
+  const manifestConnectionRun = async (): Promise<void> => {
+    // Create Manifest connection definition 
+    setRunState('Creating connection if does not exist');
+    const connectionDefinitionId = await fetchConnectionDefinitionId();
 
-    // Run Manifest connection
-    setRunState("Running connection");
-    const runLocation: string | null = await runManifestConnectionDefinition(createResponse.id!, uploadedFileName!);
+    // run manifest connection
+    setRunState('Synchronizing connection');
+    const runLocation: string | null = await runManifestConnectionDefinition(
+      connectionDefinitionId!
+    );
 
     // Get Manifest connection run results
-    if (runLocation) {
+    if (runLocation != null) {
       var interval = setInterval(async () => {
-        const runResult: ManifestRunResponse = await fetchRunResults(runLocation);
-        if (runResult.run!.state === "Completed") {
+        const runResult: ManifestRunResponse = await fetchRunResults(
+          runLocation
+        );
+        if (runResult.run!.state === 'Completed') {
           setRunState(runResult.run!.result);
           clearInterval(interval);
         }
       }, 60000);
     }
-
   };
 
   useEffect(() => {
@@ -61,19 +68,25 @@ export const FilePage = (props: RouteComponentProps) => {
 
   return (
     <div className="file-page">
-      {runState ?
+      {runState ? (
         <ProgressLinear
           labels={[runState]}
           indeterminate={runState === 'Success' || 'Error' ? true : undefined}
-          status={runState === 'Success' ? 'positive' : runState === 'Error' ? 'negative' : undefined}
+          status={
+            runState === 'Success'
+              ? 'positive'
+              : runState === 'Error'
+              ? 'negative'
+              : undefined
+          }
         />
-        :
-        <FileUpload
-          onFileDropped={fileUpload}
-        >
-          <FileUploadTemplate onChange={(e: any) => fileUpload(e.target.files)} />
+      ) : (
+        <FileUpload onFileDropped={fileUpload}>
+          <FileUploadTemplate
+            onChange={(e: any) => fileUpload(e.target.files)}
+          />
         </FileUpload>
-      }
+      )}
     </div>
   );
 };
